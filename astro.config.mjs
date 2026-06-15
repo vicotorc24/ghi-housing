fa68a4d49b241b25afc4a7123586a0b60e519a9f
Alberto V <avelasquez@unitedvirtualities.com>
2026-06-14 20:00:20 -0500

    Fix sitemap build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ghi-housing.com',
-  integrations: [sitemap()]
+  site: 'https://www.ghi-housing.com'
 });
